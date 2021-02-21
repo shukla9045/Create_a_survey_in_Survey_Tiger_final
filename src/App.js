c254e34f4565052d9c7ad0864bf2418c0326a486
Saurabh Shukla <73607833+shukla9045@users.noreply.github.com>
2021-02-21 17:57:03 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./component/Header";
 import "./styles.css";
-import MyComponent from "./component/test/select";
 import { Button } from "react-bootstrap";
 import CreateSurvey from "./component/CreateSurvey";
-import MultiSelect from "./component/CreateSurvey/multiSelect/index";
 // const handleClick = () =>{indexindex
 //     this.setState(true);
 // };
@@ -22,7 +20,6 @@ const App = () => {
   return (
     <div className="App">
       <Header />
-      <MultiSelect />
       <div className="content">
         <CreateSurvey
           click={handleClick}
