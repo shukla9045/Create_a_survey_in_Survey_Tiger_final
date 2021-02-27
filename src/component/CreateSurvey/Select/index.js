a75ca4641482645e9d65c032a560539b2cd7acee
Saurabh Shukla <73607833+shukla9045@users.noreply.github.com>
2021-02-27 12:53:05 +0530

    updated

--- src/component/CreateSurvey/Select/index.js
+++ src/component/CreateSurvey/Select/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Select from "react-select";
+import Publish from "../../Publish/index";
 import MultiSelect from "../multiSelect/index";
 import SingleSelect from "../singleSelect/index";
 const options = [
@@ -7,35 +8,60 @@ const options = [
   { value: "MultiSelect", label: "multi-Select" }
 ];
 
-const MyComponent = () => {
+const MyComponent = ({
+  collectSingledata,
+  setCollectSingleData,
+  collectMultidata,
+  setCollectMultiData,
+  setPublishFlag,
+  publishflag
+}) => {
   const [value, setValue] = useState({
     selectedOption: null
   });
   const [select, setSelect] = useState({ select: null });
-  // state = {
-  //   selectedOption: null
-  // };
+  // const [publishflag, setPublishFlag] = useState(false);
 
   const handleChange = (selectedOption) => {
     setValue({ selectedOption });
     setSelect(selectedOption.value);
     console.log(`Option selected:`, selectedOption.value);
+    // console.log(collectMultidata);
+    // console.log(collectSingledata);
   };
   const { selectedOption } = { value };
-
   return (
     <>
-      <Select
-        value={selectedOption}
-        placeholder={"Select Question Type"}
-        onChange={handleChange}
-        options={options}
-      />
-      {select == "MultiSelect" ? (
-        <MultiSelect />
-      ) : select == "SingleSelect" ? (
-        <SingleSelect />
-      ) : null}
+      {publishflag ? (
+        <Publish
+          collectSingledata={collectSingledata}
+          setCollectSingleData={setCollectSingleData}
+          collectMultidata={collectMultidata}
+          setCollectMultiData={setCollectMultiData}
+        />
+      ) : (
+        <div>
+          <Select
+            value={selectedOption}
+            placeholder={"Select Question Type"}
+            onChange={handleChange}
+            options={options}
+          />
+          {select === "MultiSelect" ? (
+            <MultiSelect
+              collectMultidata={collectMultidata}
+              setCollectMultiData={setCollectMultiData}
+              setPublishFlag={setPublishFlag}
+            />
+          ) : select === "SingleSelect" ? (
+            <SingleSelect
+              collectSingledata={collectSingledata}
+              setCollectSingleData={setCollectSingleData}
+              setPublishFlag={setPublishFlag}
+            />
+          ) : null}
+        </div>
+      )}
     </>
   );
 };
